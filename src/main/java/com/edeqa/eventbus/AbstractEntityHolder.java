bb7e59595f4a33dd2e9636bbfeb9c10d38a5eeb4
eduardm <eduardm@pantheon-inc.com>
2017-07-06 12:17:50 -0400

    Refactor to eventBus

---
+++ src/main/java/com/edeqa/eventbus/AbstractEntityHolder.java
@@ -0,0 +1,44 @@
+package com.edeqa.eventbus;
+
+/*
+ * Created by Edward Mukhutdinov (tujger@gmail.com)
+ *
+ */
+abstract public class AbstractEntityHolder<T> {
+
+    public static final String PRINT_HOLDER_NAME = "print_holder_name";
+
+    protected T context;
+
+    protected AbstractEntityHolder(T context) {
+        this.context = context;
+    }
+
+    public void setContext(T context){
+        this.context = context;
+    }
+
+    public String getType() {
+        return this.getClass().getSimpleName();
+    }
+
+    /**
+     * Will call after holder was registered.
+     */
+    public void start() {}
+
+    /**
+     * Will call before holder will be unregistered.
+     */
+    public void finish() {}
+
+    public boolean onEvent(String eventName, Object eventObject) {
+        switch(eventName) {
+            case PRINT_HOLDER_NAME:
+                System.out.println("EntityHolder name: " + getType());
+                break;
+        }
+        return true;
+    }
+
+}
---
+++ src/main/java/com/edeqa/eventbus/EventBus.java
@@ -0,0 +1,119 @@
+package com.edeqa.eventbus;
+
+/*
+ * Created by Edward Mukhutdinov (tujger@gmail.com)
+ *
+ */
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+@SuppressWarnings({"WeakerAccess", "unused"})
+public class EventBus {
+
+    public static final String DEFAULT_NAME = "default";
+
+    private static Map<String,ArrayList<AbstractEntityHolder>> holders = new HashMap<>();
+
+    private String eventBusName;
+
+    private static ExecutorService executor = Executors.newSingleThreadExecutor();
+
+    public EventBus() {
+        this(DEFAULT_NAME);
+    }
+
+    public EventBus(String eventBusName) {
+        this.eventBusName = eventBusName;
+        holders.put(eventBusName, new ArrayList<AbstractEntityHolder>());
+    }
+
+    public void register(AbstractEntityHolder holder) {
+        holders.get(eventBusName).add(holder);
+        try {
+            holder.start();
+        } catch (Exception e) {
+            System.err.println("with eventBusName '" + eventBusName + "' and holder " + holder);
+            e.printStackTrace();
+        }
+    }
+
+    public void unregister(AbstractEntityHolder holder) {
+        try {
+            holder.finish();
+        } catch (Exception e) {
+            System.err.println("with eventBusName '" + eventBusName + "' and holder " + holder);
+            e.printStackTrace();
+        }
+        holders.get(eventBusName).remove(holder);
+    }
+
+    public void post(String eventName) {
+        post(eventName, null);
+    }
+
+    public void post(String eventName, Object eventObject) {
+        post(eventBusName, eventName, eventObject);
+    }
+
+    public static void post(final String eventBusName, final String eventName, final Object eventObject) {
+        executor.submit(new Runnable() {
+            @Override
+            public void run() {
+                postSync(eventBusName, eventName, eventObject);
+            }
+        });
+    }
+
+    public static void postSync(String eventBusName, String eventName, Object eventObject) {
+        for (AbstractEntityHolder x : holders.get(eventBusName)) {
+            try {
+                if (!x.onEvent(eventName, eventObject)) {
+                    break;
+                }
+            } catch (Exception e) {
+                System.err.println("with eventBusName '" + eventBusName + ", holder '"+ x.getType() +"' and eventName '" + eventName + "'");
+                e.printStackTrace();
+            }
+        }
+    }
+
+
+    /**
+     * Will post event/object to each holder in eventBus entirely.
+     */
+    public static void postAll(String eventName, Object eventObject) {
+        for(Map.Entry<String,ArrayList<AbstractEntityHolder>> x: holders.entrySet()) {
+            post(x.getKey(), eventName, eventObject);
+        }
+    }
+
+    public static void postAll(String eventName) {
+        postAll(eventName, null);
+    }
+
+    /**
+     * Will call holder.finish() on each holder before clear eventBus.
+     */
+    public void clear() {
+        for(AbstractEntityHolder holder: holders.get(eventBusName)) {
+            holder.finish();
+        }
+        holders.remove(eventBusName);
+    }
+
+    /**
+     * Will call holder.finish() on each holder before clear eventBus.
+     */
+    public static void clearAll() {
+        for(Map.Entry<String,ArrayList<AbstractEntityHolder>> bus: holders.entrySet()) {
+            for(AbstractEntityHolder holder: bus.getValue()) {
+                holder.finish();
+            }
+        }
+        holders = new HashMap<>();
+    }
+}
