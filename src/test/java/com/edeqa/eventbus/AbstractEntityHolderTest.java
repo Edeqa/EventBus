14ee2688c0811dee801df9cdbd7574b698037d21
Tujger <tujger@gmail.com>
2017-09-20 01:23:10 -0400

    Tests

---
+++ src/test/java/com/edeqa/eventbus/AbstractEntityHolderTest.java
@@ -0,0 +1,52 @@
+package com.edeqa.eventbus;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created 9/20/17.
+ */
+public class AbstractEntityHolderTest {
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @Test
+    public void setContext() throws Exception {
+
+    }
+
+    @Test
+    public void getType() throws Exception {
+
+    }
+
+    @Test
+    public void start() throws Exception {
+
+    }
+
+    @Test
+    public void finish() throws Exception {
+
+    }
+
+    @Test
+    public void events() throws Exception {
+
+    }
+
+    @Test
+    public void onEvent() throws Exception {
+
+    }
+
+    @Test
+    public void setLoggingLevel() throws Exception {
+
+    }
+
+}
---
+++ src/test/java/com/edeqa/eventbus/EventBusTest.java
@@ -0,0 +1,142 @@
+package com.edeqa.eventbus;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created 9/20/17.
+ */
+public class EventBusTest {
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @Test
+    public void register() throws Exception {
+
+    }
+
+    @Test
+    public void registerOrUpdate() throws Exception {
+
+    }
+
+    @Test
+    public void update() throws Exception {
+
+    }
+
+    @Test
+    public void unregister() throws Exception {
+
+    }
+
+    @Test
+    public void unregister1() throws Exception {
+
+    }
+
+    @Test
+    public void getHolders() throws Exception {
+
+    }
+
+    @Test
+    public void getHolders1() throws Exception {
+
+    }
+
+    @Test
+    public void getHoldersList() throws Exception {
+
+    }
+
+    @Test
+    public void getHoldersList1() throws Exception {
+
+    }
+
+    @Test
+    public void post() throws Exception {
+
+    }
+
+    @Test
+    public void post1() throws Exception {
+
+    }
+
+    @Test
+    public void post2() throws Exception {
+
+    }
+
+    @Test
+    public void post3() throws Exception {
+
+    }
+
+    @Test
+    public void postRunnable() throws Exception {
+
+    }
+
+    @Test
+    public void postRunnable1() throws Exception {
+
+    }
+
+    @Test
+    public void postSync() throws Exception {
+
+    }
+
+    @Test
+    public void postAll() throws Exception {
+
+    }
+
+    @Test
+    public void postAll1() throws Exception {
+
+    }
+
+    @Test
+    public void postAll2() throws Exception {
+
+    }
+
+    @Test
+    public void clear() throws Exception {
+
+    }
+
+    @Test
+    public void clearAll() throws Exception {
+
+    }
+
+    @Test
+    public void setRunner() throws Exception {
+
+    }
+
+    @Test
+    public void setMainRunner() throws Exception {
+
+    }
+
+    @Test
+    public void getHolder() throws Exception {
+
+    }
+
+    @Test
+    public void inspect() throws Exception {
+
+    }
+
+}
